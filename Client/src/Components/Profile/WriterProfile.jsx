da4df53b02c75d6b5700e500fa4bb84a17edcd26
shubham09anand <shubhamsheelam@gmail.com>
2024-08-15 13:52:41 +0530

    init

--- Client/src/Components/Profile/WriterProfile.jsx
+++ Client/src/Components/Profile/WriterProfile.jsx
@@ -77,6 +77,10 @@ const WriterProfile = () => {
           }
      };
 
+     const showDeleteOpt = (index) =>{
+          setShowDelete(index)
+     }
+
      return (
           <div className='flex flex-col-reverse lg:flex-row w-screen lg:px-40 mt-5'>
                <ToastContainer />
@@ -113,14 +117,14 @@ const WriterProfile = () => {
                                                   <div className="flex place-content-center items-center relative px-4 border-opacity-0 hover:border-opacity-100 duration-200 cursor-pointer">
                                                        <div className={`shadow-[1px_1px_1px_black] mr-4 text-center flex place-content-center border-r-2 ${showDelete === key ? 'flex' : 'hidden'}`}>
                                                             <h3 className="font text-lg py-1 px-2 font-bold text-gray-700 pr-3">Are You sure ?</h3>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="white" className="size-6 my-auto bg-red-600 mr-4 rounded-sm">
+                                                            <svg onClick={()=>showDeleteOpt(null)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="white" className="size-6 my-auto bg-red-600 mr-4 rounded-sm">
                                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                                                             </svg>
                                                             <svg onClick={() => deleteBlog(items?._id, key)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="white" className="size-6 my-auto bg-green-600 mr-2 rounded-sm">
                                                                  <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                                                             </svg>
                                                        </div>
-                                                       <div name="showDelete" className='flex place-content-center items-center font-thin space-x-4 sm:text-lg'>
+                                                       <div onClick={()=>showDeleteOpt(key)} name="showDelete" className='flex place-content-center items-center font-thin space-x-4 sm:text-lg'>
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-5">
                                                                  <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                             </svg>
