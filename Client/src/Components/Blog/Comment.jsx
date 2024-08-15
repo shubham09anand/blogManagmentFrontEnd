a93d87400d4bc62556c790de6a221636aa96562a
shubham09anand <shubhamsheelam@gmail.com>
2024-08-15 13:55:26 +0530

    init

--- Client/src/Components/Blog/Comment.jsx
+++ Client/src/Components/Blog/Comment.jsx
@@ -40,7 +40,7 @@ const Comment = () => {
                 comments.map((item,key) => (
                     <div key={key} className="w-full p-6 text-base bg-white border-b">
                         <div className="flex justify-between items-center mb-2">
-                            <Link to={`/profile/${item._id}`} className="flex items-center">
+                            <Link to={`/profile/${item.authorId}`} className="flex items-center">
                                 <p className="inline-flex items-center mr-3 font-semibold text-sm text-gray-900">
                                     <img className="mr-2 w-6 h-6 rounded-full border-2 border-black" src={item?.photo?.trim() !== "" ? item?.photo : noProfilePhoto} alt="Profile" />
                                     <span>{item?.firstName} {item?.lastName}</span>
