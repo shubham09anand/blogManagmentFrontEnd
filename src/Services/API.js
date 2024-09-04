44a671c446bf9e836a572be1aba063467ea6734c
shubham09anand <shubhamsheelam@gmail.com>
2024-09-04 10:27:13 +0530

    init

--- src/Services/API.js
+++ src/Services/API.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-// const API = axios.create({baseURL: 'http://13.127.165.62:4000/'});
 const API = axios.create({baseURL: 'http://13.200.187.11:4000/'});
 
 API.interceptors.request.use((req)=>{
