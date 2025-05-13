811e4f2ff846874ad34496a73984697f0d8da005
shubham09anand <shubhamsheelam@gmail.com>
2025-05-13 17:52:29 +0530

    inti

--- src/Components/Blog/MakeComment.jsx
+++ src/Components/Blog/MakeComment.jsx
@@ -29,42 +29,42 @@ const MakeComment = () => {
 
      const makeComment = async () => {
           if (!validateInputs()) {
-              return;
+               return;
           }
-          setLoading(true); 
+          setLoading(true);
           const CurrentTime = getCurrentTime();
-      
+
           const commentData = { authorId: userId, blogId: blogId, comment: comment, createdAt: CurrentTime,};
-          
+
           try {
-              if (blogId && userId) {
-                  const response = await API.post("/makeComments", commentData);
-                  
-                  if (response.data.response.success) {
-                      toast.success("Comment is saved");
-                      setComment("");
-                  }
-              }
+               if (blogId && userId) {
+                    const response = await API.post("/makeComments", commentData);
+
+                    if (response.data.response.success) {
+                         toast.success("Comment is saved");
+                         setComment("");
+                    }
+               }
           } catch (error) {
-              console.log(error);
-              toast.error("Failed to post comment.");
+               console.log(error);
+               toast.error("Failed to post comment.");
           } finally {
-              setTimeout(() => {
-                  setLoading(false);
-              }, 5000);
+               setTimeout(() => {
+                    setLoading(false);
+               }, 5000);
           }
-      };
-      
+     };
+
 
      return (
           <section className="w-full not-format my-6">
-               <ToastContainer/>
+               <ToastContainer />
                <form className="mb-6 w-full">
                     <div className="py-2 mb-4 bg-white rounded-lg rounded-t-lg  border-gray-200">
                          <label htmlFor="comment" className="sr-only">Your comment</label>
-                         <textarea  onChange={(e) => setComment(e.target.value)}  value={comment}  id="comment"  rows="6"  className="px-3 py-2 w-full resize-none outline-none text-sm text-gray-900 focus:outline-[#34ab45] border focus:ring-0"  placeholder="Write a comment..."  required disabled={loading}></textarea>
+                         <textarea onChange={(e) => setComment(e.target.value)} value={comment} id="comment" rows="6" className="px-3 py-2 w-full resize-none outline-none text-sm text-gray-900 focus:outline-[#34ab45] border focus:ring-0" placeholder="Write a comment..." required disabled={loading}></textarea>
                     </div>
-                    <button  type="button" onClick={makeComment} className={`inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-green-600 rounded-sm focus:ring-4 focus:ring-primary-200 hover:bg-primary-800 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={loading} >
+                    <button type="button" onClick={makeComment} className={`inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-green-600 rounded-sm focus:ring-4 focus:ring-primary-200 hover:bg-primary-800 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={loading} >
                          {loading ? 'Posting...' : 'Post comment'}
                     </button>
                </form>
--- src/Services/API.js
+++ src/Services/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const API = axios.create({baseURL: 'http://13.202.160.2:4000/'});
 const API = axios.create({baseURL: 'https://apiinsider.shubham09anand.in/'});
-// const API = axios.create({baseURL: 'http://192.168.1.6:4000/'});
+// const API = axios.create({baseURL: 'http://127.0.0.1:4000/'});
 
 API.interceptors.request.use((req)=>{
      return req;
